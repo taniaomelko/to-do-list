a415b0480eef5fa5a7cd7d82345496eb819535ca
Tania Omelko <tanyaomelko39@gmail.com>
2024-04-27 00:37:58 +0300

    rework router

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,25 @@
 import React from 'react';
 import './App.scss';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { Header } from './components/Header/Header';
+import { RouterProvider, createBrowserRouter, createRoutesFromElements, Route } from 'react-router-dom';
+import { Root } from './components/Root/Root';
 import { Home } from './components/Home/Home';
 import { Todos } from './components/Todos/Todos';
 import { Trash } from './components/Trash/Trash';
 import { EViewMode } from './types/EViewMode';
 
 export const App: React.FC = () => {
-  const basename = process.env.PUBLIC_URL;
+  const router = createBrowserRouter(createRoutesFromElements(
+    <Route path="/" element={ <Root/> }>
+      <Route path="/" element={<Home />} />
+      <Route path={EViewMode.All} element={<Todos />} />
+      <Route path={EViewMode.Deleted} element={<Trash />} />
+    </Route>
+    ), {
+      basename: process.env.PUBLIC_URL
+    }
+  );
 
   return (
-    <Router basename={basename}>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path={EViewMode.All} element={<Todos />} />
-        <Route path={EViewMode.Deleted} element={<Trash />} />
-      </Routes>
-    </Router>
+    <RouterProvider router={router} />
   );
 }
